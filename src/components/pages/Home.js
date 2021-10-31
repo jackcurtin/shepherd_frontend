e40606ef8632c8ef5e701e29280e6469d180cd63
Jack Curtin <jackcurtin94@gmail.com>
2021-10-31 15:23:07 -0500

    added login inputs

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,19 @@
 function Home() {
     return ( 
-        <p> Welcome to Shepherd </p>
+        <div>
+            <p> Welcome to Shepherd </p>
+            <form>
+                <label>
+                    email
+                    <input></input>
+                </label>
+                <label>
+                    password
+                    <input></input>
+                </label>
+                <input type="submit" value="Submit"></input>
+            </form>
+        </div>
      );
 }
 export default Home;
